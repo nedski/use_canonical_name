b66d4f9f87e335c8e864cb0de25668dcb1a3f30d
Neil Kohl <neil@kohlweb.com>
2010-07-08 16:51:48 -0400

    Added logging interface Ilogger.cs and implementation Acp.Logger.cs

---
+++ Acp.Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+/// <summary>
+/// Summary description for Acp.Logger
+/// </summary>
+namespace Acp
+{
+    public class Logger : ILogger
+    {
+
+        private string _sourceName = String.Empty;
+
+        public Logger(string SourceName)
+        {
+           _sourceName = SourceName;
+
+        }
+        public void Debug(string text)
+        {
+            EventLog.WriteEntry(_sourceName, text, EventLogEntryType.Information);
+        }
+
+        public void Warn(string text)
+        {
+            EventLog.WriteEntry(_sourceName, text, EventLogEntryType.Warning);
+
+        }
+
+        public void Error(string text)
+        {
+            EventLog.WriteEntry(_sourceName, text, EventLogEntryType.Error );
+        }
+
+        public void Error(string text, Exception ex)
+        {
+            Error(text);
+            Error(ex.StackTrace);
+        }
+    }
+}
---
+++ Ilogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+interface ILogger
+{
+
+    void Debug(string text);
+    void Warn(string text);
+    void Error(string text);
+    void Error(string text, Exception ex);
+
+}
